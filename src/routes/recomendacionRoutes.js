fcba566c9c1e775e41d350c5c1f460d17ed0b8b0
Kamiro <prietocamilo21@gmail.com>
2025-11-02 21:42:13 -0500

    ADD>> Configuración de ruta Recomendaciones
    Se agregó el archivo js para la ruta en express.

---
+++ src/routes/recomendacionRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const recomendacionController = require('../controllers/recomendacionController');
+
+// Crear una nueva recomendación
+router.post('/', recomendacionController.crearRecomendacion);
+
+// Obtener todas las recomendaciones
+router.get('/', recomendacionController.obtenerRecomendaciones);
+
+// Obtener una recomendación por ID
+router.get('/:id', recomendacionController.obtenerRecomendacion);
+
+// Eliminar una recomendación por ID
+router.delete('/:id', recomendacionController.eliminarRecomendacion);
+
+module.exports = router;
